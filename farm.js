d97a220aa17d9e9ae5486d4c0d3d16b3a6798780
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-08-11 16:45:19 +0200

    Declaration of empty functions

--- farm.js
+++ farm.js
@@ -1,1 +1,36 @@
-module.exports = {};
+const corn = {
+  name: "corn",
+  yield: 30,
+  factors: {
+    sun: {
+      low: -50,
+      medium: 0,
+      high: 50
+    }
+  }
+};
+
+const environmentFactors = {
+  sun: "low"
+};
+
+const getYieldForPlant = () => {};
+
+const getYieldForCrop = () => {};
+
+const getTotalYield = () => {};
+
+const getRevenueForCrop = () => {};
+
+const getProfitForCrop = () => {};
+
+const getTotalProfit = () => {};
+
+module.exports = {
+  getYieldForPlant,
+  getYieldForCrop,
+  getTotalYield,
+  getRevenueForCrop,
+  getProfitForCrop,
+  getTotalProfit
+};
